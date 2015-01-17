25b4e609c818e69a4a9d5e5acd1709e08134fad0
normies <simon.schindelmann@gmail.com>
2015-01-17 11:02:15 +0100

    structur changed

--- KickerManager.sql
+++ KickerManager.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.2.6deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 26. Nov 2014 um 08:41
--- Server Version: 5.5.40-0ubuntu0.14.04.1
--- PHP-Version: 5.5.9-1ubuntu4.5
+-- Erstellungszeit: 17. Jan 2015 um 10:59
+-- Server Version: 5.5.40-0ubuntu1
+-- PHP-Version: 5.5.12-2ubuntu4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,12 +27,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `role` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `role` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `role_description` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `role` (`role`)
+  `role_description` varchar(255) CHARACTER SET latin1 DEFAULT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
@@ -50,60 +47,57 @@ INSERT INTO `role` (`id`, `role`, `role_description`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`id` int(10) unsigned NOT NULL,
   `login_name` varchar(255) COLLATE utf8_bin NOT NULL,
   `login_password` varchar(255) COLLATE utf8_bin NOT NULL,
   `first_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `last_name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `enable` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `login_name` (`login_name`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `id_2` (`id`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+  `role_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
 
 --
 -- Daten für Tabelle `user`
 --
 
-INSERT INTO `user` (`id`, `login_name`, `login_password`, `first_name`, `last_name`, `email`, `enable`) VALUES
-(1, 'schindy', 'test', 'Simon', 'Schindelmann', 'test@test.de', 1),
-(2, 'schindy2', 'test2', 'Simon2', 'Schindelmann2', 'test@test.de2', 0),
-(3, 'schindy23', 'test23', 'Simon23', 'Schindelmann23', 'test@test.de23', 0);
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `user_roles`
---
-
-CREATE TABLE IF NOT EXISTS `user_roles` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `role_id` int(10) unsigned NOT NULL DEFAULT '2',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`user_id`),
-  UNIQUE KEY `role_id` (`role_id`),
-  KEY `user_id_2` (`user_id`),
-  KEY `user_id_3` (`user_id`),
-  KEY `user_id_4` (`user_id`),
-  KEY `user_id_5` (`user_id`),
-  KEY `user_id_6` (`user_id`),
-  KEY `user_id_7` (`user_id`),
-  KEY `user_id_8` (`user_id`),
-  KEY `user_id_9` (`user_id`),
-  KEY `role_id_2` (`role_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+INSERT INTO `user` (`id`, `login_name`, `login_password`, `first_name`, `last_name`, `email`, `enable`, `role_id`) VALUES
+(1, 'schindy', 'test', 'Simon', 'Schindelmann', 'test@test.de', 1, 1),
+(2, 'schindy2', 'test2', 'Simon2', 'Schindelmann2', 'test@test.de2', 1, 2),
+(3, '', '', '', '', '', 0, 0),
+(4, 'test', 'tesfsdfsdfsd', 'test123', 'test123', 'simon@web.de', 0, 0),
+(7, 'test222', 'test', 'tests', 'tststs', 'tst@webde', 0, 0);
 
 --
--- Daten für Tabelle `user_roles`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `user_roles` (`id`, `user_id`, `role_id`) VALUES
-(3, 1, 1);
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`), ADD UNIQUE KEY `role` (`role`);
 
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `login_name` (`login_name`), ADD UNIQUE KEY `email` (`email`), ADD UNIQUE KEY `id_2` (`id`), ADD KEY `id` (`id`), ADD KEY `role_id` (`role_id`), ADD KEY `role_id_2` (`role_id`), ADD KEY `role_id_3` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
